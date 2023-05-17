b73558dfb01a5023755e4b4633f8934782580f5a
Caspar2318 <108163945+Caspar2318@users.noreply.github.com>
2023-05-18 08:35:00 +1000

    more details

--- client/src/components/CustomButton.jsx
+++ client/src/components/CustomButton.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import styles from "../styles";
 
-const CustomButton = ({ title, handleClick, restType }) => {
+const CustomButton = ({ title, handleClick, restStyles }) => {
   return (
     <button
       type="button"
-      className={`${styles.btn} ${restType}`}
+      className={`${styles.btn} ${restStyles}`}
       onClick={handleClick}
     >
       {title}
